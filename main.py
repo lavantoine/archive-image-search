68d4801af81cf7c5e9a8e79be8616115f35d63a3
lavantoine <antoine.lavacquery@proton.me>
2025-10-19 16:07:49 +0200

    Add scan images from folder for collection fill

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from pathlib import Path
 import chromadb
 from chromadb import Documents, EmbeddingFunction, Embeddings
 from pprint import pprint
+from tqdm import tqdm
 
 class EfficientNetImageEmbedding(EmbeddingFunction[Documents]):
-    def __init__(self, model_name: str = 'google/efficientnet-b7', device: str = 'cpu') -> None:
+    def __init__(self, model_name: str = 'google/efficientnet-b0', device: str = 'mps') -> None:
         super().__init__()
         self.model_name = model_name
         self.device = device
@@ -30,39 +31,54 @@ class EfficientNetImageEmbedding(EmbeddingFunction[Documents]):
     
     def __call__(self, input: list[Path | str]) -> Embeddings:
         all_embeddings = []
-        for img_path in input:
+        print(f"🔹 Embedding generation for {len(input)} images...")
+
+        for img_path in tqdm(input, desc="Embedding images", unit="img"):
             pixel_values = self.load_process_image(img_path)['pixel_values']
             with torch.no_grad():
                 outputs = self.model(pixel_values=pixel_values)
-            emb = outputs.pooler_output[0].numpy()  # vecteur numpy
+            emb = outputs.pooler_output[0].cpu().numpy()  # vecteur numpy
             all_embeddings.append(emb.tolist())
+        
+        print("✅ Embeddings finished.")
         return all_embeddings
 
 if __name__ == '__main__':
-    script_dir = Path(__file__).parent
-    img_path = script_dir / '../data/'
-    
     # For TPU acceleration
     device = "mps" if torch.backends.mps.is_available() else "cpu"
     device = 'cpu'
     model_name = 'google/efficientnet-b7'
 
-    images = [script_dir / '../data/base/mona-lisa-0.jpg', script_dir / '../data/mona-lisa-1.jpg']
+    script_dir = Path(__file__).parent
+    data_dir = script_dir / 'data/portrait-0.1k'
+    files = list(data_dir.rglob('*.jpg'))
+    print(f"{len(files)} images trouvées.")
     
-    embed_function = EfficientNetImageEmbedding()
+    embed_function = EfficientNetImageEmbedding(device='cpu')
 
     chroma_client = chromadb.Client()
-    collection = chroma_client.create_collection(name='my_collection', embedding_function=embed_function)
+    collection = chroma_client.create_collection(
+        name='my_collection',
+        embedding_function=embed_function
+    )
     
-    embeddings = embed_function(images)
     
-    collection.upsert(ids=['id0', 'id1'], embeddings=embeddings)
+    ids = [f"id_{i}" for i in range(len(files))]
+    
+    embeddings = embed_function(files)
+    
+    collection.upsert(ids=ids, embeddings=embeddings)
+    
+    print(f"{len(embeddings)} embeddings ajoutés à la collection.")
+    
+    query_image = [script_dir / 'data/test/mona-lisa-test.jpg']
     
     results = collection.query(
-    query_embeddings=embed_function([script_dir / '../data/base/mona-lisa-3-crop.jpeg']),
-    n_results=2,
+    query_embeddings=embed_function(query_image),
+    n_results=5,
     include=["embeddings", "distances"]) # type: ignore
     
+    print()
     pprint(results)
 
 # def cosine_similarity(a, b):
